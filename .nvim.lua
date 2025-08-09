7d3f0cccb491f101b529b95eb1a50c627f6c7f4b
Emanuel Särnhammar <emanuel@quaqqer.com>
2025-08-09 10:26:05 +0200

    chore: remove unused nvim conf

--- .nvim.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.config('lua_ls', {
-   settings = {
-      Lua = {
-         workspace = {
-            library = { './misc/lua-libs/lua-ls-cc-tweaked/library' },
-         },
-      },
-   },
-})
